aee881c4777b266398a1c09fea676fc9bd5078ab
Krishna <gkrrish.11@gmail.com>
2024-06-28 14:38:31 +0530

    Query optimization is pending

--- src/main/java/com/batch/service/NewspaperDistributionService.java
+++ src/main/java/com/batch/service/NewspaperDistributionService.java
@@ -9,16 +9,18 @@ import com.batch.repository.BatchJobRepository;
 
 @Service
 public class NewspaperDistributionService {
-	
-	private final int BEFORE_TIME_IN_MINUTES = 12;
+
+	private final int BEFORE_TIME_IN_MINUTES = 5;
 
 	@Autowired
 	BatchJobRepository batchJobRepository;
 
 	public String getCurrentTimeBatchId() {
-		Optional<Long> currentBatchId = batchJobRepository.findCurrentBatchId(BEFORE_TIME_IN_MINUTES);
-		
-		return currentBatchId.isPresent() ? "Batch ID: " + currentBatchId.get().toString() : "Sorry, we did not meet the time.";
-	}
+		Optional<Long> currentBatchId = batchJobRepository.findCurrentBatchId(BEFORE_TIME_IN_MINUTES);//need to validate this query for different scenarios
 
+		if (currentBatchId.isPresent()) {
+			return currentBatchId.get().toString();
+		} else 
+			return "Sorry ! Our radars were not able to be found near the delivery scheduled time.";
+		}
 }
